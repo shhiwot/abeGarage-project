076fcc62d7053a11f09703a1d139843f3caa6077
MeklitMathewos <mathewosmeklit@gmail.com>
2024-08-31 01:20:09 +0300

    the first commit home jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,17 @@
 // Import react 
 import React from 'react';
 // Import the Routes and Route components from react-router 
-import { Routes } from "react-router";
-// Import the page components 
-// import Home from "./markup/pages/Home";
-// import Login from "./markup/pages/Login";
-// import AddEmployee from './markup/pages/admin/AddEmployee';
+import { Routes, Route } from "react-router";
 
+import Home from "./markup/pages/Home";
 // Import the css files 
 import "./assets/template_assets/css/bootstrap.css";
 import "./assets/template_assets/css/style.css";
 import "./assets/template_assets/css/responsive.css";
 import "./assets/template_assets/css/color.css";
 
-// Import the custom css file 
+// // Import the custom css file
+import "./assets/styles/custom copy.css";
 import "./assets/styles/custom.css";
 
 // Import the Header component 
@@ -24,10 +22,9 @@ import Footer from './markup/components/Footer/Footer';
 function App() {
   return (
     <>
-
       <Header />
       <Routes>
-        
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </>
