5976ba2ec69d74baf1e75b686801130e8b4f9333
shhiwot <shhiwot14@gmail.com>
2024-08-31 18:29:36 -0500

    add login page

--- Frontend/src/markup/pages/Login.jsx
+++ Frontend/src/markup/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // Import the Login component
-import LoginForm from "../components/LoginForm/LoginForm";
+import LoginForm from "../components/LoginForm1/LoginForm";
 function Login() {
   return (
     <div>
